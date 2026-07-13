2916ff46e50876aa6350ddf68b7a564c7a3cf4f8
the6fallenangel <alirezaa80m@gmail.com>
2026-07-14 02:31:28 +0330

    feat: add graceful shutdown to REST API server

--- cmd/todo-api/main.go
+++ cmd/todo-api/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 	"todo/internal/config"
 	"todo/internal/storage"
 )
@@ -19,9 +25,29 @@ func main() {
 	mux := http.NewServeMux()
 	registerRoutes(mux, store)
 
-	addr := ":8080"
-	fmt.Println("listening on", addr)
-	if err := http.ListenAndServe(addr, mux); err != nil {
-		log.Fatal(err)
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
 	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	go func() {
+		fmt.Println("listening on", server.Addr)
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatal("server error:", err)
+		}
+	}()
+
+	<-ctx.Done()
+	fmt.Println("shutdown signal received")
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		log.Fatal("graceful shutdown failed:", err)
+	}
+	fmt.Println("server stopped cleanly")
 }
